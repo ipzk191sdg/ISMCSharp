4e8e6e9d43487345efd6cdd47d23985e7824f1a1
Daria <wrndwrld@gmail.com>
2019-04-23 18:51:04 +0300

    Завдання 1 форма

---
+++ WinForm01/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WinForm01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculate = new System.Windows.Forms.Button();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.labelA = new System.Windows.Forms.Label();
+            this.labelB = new System.Windows.Forms.Label();
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(64, 114);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(84, 23);
+            this.calculate.TabIndex = 0;
+            this.calculate.Text = "Розрахувати";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(73, 21);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 20);
+            this.textBoxA.TabIndex = 1;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(73, 47);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 20);
+            this.textBoxB.TabIndex = 2;
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(73, 73);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 20);
+            this.textBoxX.TabIndex = 3;
+            // 
+            // labelA
+            // 
+            this.labelA.AutoSize = true;
+            this.labelA.Location = new System.Drawing.Point(32, 24);
+            this.labelA.Name = "labelA";
+            this.labelA.Size = new System.Drawing.Size(25, 13);
+            this.labelA.TabIndex = 4;
+            this.labelA.Text = "a = ";
+            // 
+            // labelB
+            // 
+            this.labelB.AutoSize = true;
+            this.labelB.Location = new System.Drawing.Point(32, 50);
+            this.labelB.Name = "labelB";
+            this.labelB.Size = new System.Drawing.Size(25, 13);
+            this.labelB.TabIndex = 5;
+            this.labelB.Text = "b = ";
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Location = new System.Drawing.Point(32, 76);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(24, 13);
+            this.labelX.TabIndex = 6;
+            this.labelX.Text = "x = ";
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.Location = new System.Drawing.Point(32, 150);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(0, 13);
+            this.labelY.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(206, 188);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.labelB);
+            this.Controls.Add(this.labelA);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.calculate);
+            this.Name = "Form1";
+            this.Text = "Завдання 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.Label labelA;
+        private System.Windows.Forms.Label labelB;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelY;
+    }
+}
+
---
+++ WinForm01/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBoxA.Text) || String.IsNullOrEmpty(textBoxB.Text) || String.IsNullOrEmpty(textBoxX.Text))
+            {
+                MessageBox.Show("Помилка. Введiть значення.");
+            }
+            else
+            {
+                double a, b, x;
+                if (!Double.TryParse(textBoxA.Text, out a))
+                {
+                    MessageBox.Show("Помилка. Введiть значення a.");
+                }
+                a = Convert.ToDouble(textBoxA.Text);
+                if (!Double.TryParse(textBoxB.Text, out b))
+                {
+                    MessageBox.Show("Помилка. Введiть значення b.");
+                }
+                b = Convert.ToDouble(textBoxB.Text);
+                if (!Double.TryParse(textBoxX.Text, out x))
+                {
+                    MessageBox.Show("Помилка. Введiть значення x.");
+                }
+                x = Convert.ToDouble(textBoxX.Text);
+                double y = 2.4 * Math.Abs((x * x + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + Math.Pow(10, -2) * (x - b);
+                labelY.Text = "y = " + y.ToString();
+            }
+        }
+    }
+}
